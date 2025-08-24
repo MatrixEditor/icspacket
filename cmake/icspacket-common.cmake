2683a26d97424b3d76b300a6414b78c9630f6cbc
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-08-24 11:22:22 +0200

    Add ASN.1 sources for ISO presentation layer (ISO 8823) ---
    proto.iso_pres:
    + Add ASN.1 definition file and C sources
    + Add extension to CMakeLists
    
    cmake-common:
    + Update add_asn1_extension parameters to named variables

--- cmake/icspacket-common.cmake
+++ cmake/icspacket-common.cmake
@@ -7,14 +7,14 @@
 #
 # # Minimal usage: build an extension from MOD_DIR sources
 # add_asn1_extension(
-#     mymodule                                      # Python extension name
-#     ${ICS_SOURCE_DIR}/proto/mymodule/_mymodule    # Sources directory
-#     proto/mymodule                                # Install subdirectory
+#     NAME mymodule                                      # Python extension name
+#     DIR ${ICS_SOURCE_DIR}/proto/mymodule/_mymodule     # Sources directory
+#     INSTALL proto/mymodule                             # Install subdirectory
 # )
-function(add_asn1_extension MOD_NAME MOD_DIR INSTALL_SUBDIR)
+function(add_asn1_extension)
     # Parse function arguments
-    set(options)              # (reserved for future boolean flags)
-    set(one_value_args)       # (reserved for single-value args)
+    set(options) # (reserved for future boolean flags)
+    set(one_value_args NAME DIR INSTALL)
     set(multi_value_args EXTRA_SOURCES INCLUDES DEFINITIONS)
 
     cmake_parse_arguments(
@@ -25,9 +25,9 @@ function(add_asn1_extension MOD_NAME MOD_DIR INSTALL_SUBDIR)
         ${ARGN}
     )
 
-    file(GLOB MOD_SOURCES "${MOD_DIR}/*.c")
+    file(GLOB MOD_SOURCES "${ASN1_DIR}/*.c")
     python_add_library(
-        ${MOD_NAME}
+        ${ASN1_NAME}
         MODULE
         ${ICS_BASE_SOURCES}
         ${MOD_SOURCES}
@@ -35,25 +35,25 @@ function(add_asn1_extension MOD_NAME MOD_DIR INSTALL_SUBDIR)
         WITH_SOABI
     )
 
-    message(STATUS "======================= [${MOD_NAME}] =======================")
-    message(STATUS "Adding ASN.1 extension: ${MOD_NAME} (sources in ${MOD_DIR})")
+    message(STATUS "======================= [${ASN1_NAME}] =======================")
+    message(STATUS "Adding ASN.1 extension: ${ASN1_NAME} (sources in ${ASN1_DIR})")
 
     target_include_directories(
-        ${MOD_NAME}
+        ${ASN1_NAME}
         PRIVATE
         "${ICS_INCLUDE_DIR}/skeletons"
-        "${MOD_DIR}"
+        "${ASN1_DIR}"
         ${ASN1_INCLUDES}
     )
     if(ASN1_DEFINITIONS)
-        target_compile_definitions(${MOD_NAME} PRIVATE ${ASN1_DEFINITIONS})
+        target_compile_definitions(${ASN1_NAME} PRIVATE ${ASN1_DEFINITIONS})
         message(STATUS "Extra definitions: ${ASN1_DEFINITIONS}")
     endif()
 
-    # Install into the Python package namespace under INSTALL_SUBDIR
+    # Install into the Python package namespace under ASN1_INSTALL_DIR
     install(
-        TARGETS ${MOD_NAME}
-        DESTINATION ${SKBUILD_PROJECT_NAME}/${INSTALL_SUBDIR}
+        TARGETS ${ASN1_NAME}
+        DESTINATION ${SKBUILD_PROJECT_NAME}/${ASN1_INSTALL}
     )
 endfunction()
 
