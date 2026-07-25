c1b0c5a82b2d86d34970fbae8c1e8b88ccd3f724
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2026-07-25 22:22:35 +0200

    chore: prepare iec104 implementation

---
+++ src/icspacket/proto/iec104/__init__.py
@@ -0,0 +1,26 @@
+# This file is part of icspacket.
+# Copyright (C) 2025-present  MatrixEditor @ github
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""\
+**IEC 60870-5-104 - Network access for IEC 60870-5-101 using standard
+transport profiles**
+
+This package provides a pure-Python implementation of IEC 60870-5-104: the
+TCP/IP profile that carries IEC 60870-5-101's application-layer message
+structure (ASDUs) over a routable network transport instead of a serial
+FT1.2 link.
+
+.. versionadded:: 0.3.0
+"""
